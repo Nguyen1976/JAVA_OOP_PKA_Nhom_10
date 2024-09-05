825506d61c159ab8b6ef4fca5ef612ca16d14809
Nguyen1976 <Nguyen2202794@gmail.com>
2024-09-05 15:46:27 +0700

    fix practice 1 in main

---
+++ Practice_1_Nguyen/Assignment2.java
@@ -0,0 +1,13 @@
+public class Assignment2 {
+    
+    public static void main(String[] args) {
+        Number n1 = new Number();
+        Number n2 = new Number();
+        n1.i = 2;
+        n2.i = 5;
+        n1 = n2; // n1 n2 deu tham chieu den cung 1 doi tuong
+        n2.i = 10;// what is n1.i? = 10
+        n1.i = 20;// what is n2.i? = 20
+    }
+    
+}
---
+++ Practice_1_Nguyen/PassObject.java
@@ -0,0 +1,12 @@
+public class PassObject {
+    static void f(Number m){ //phuong thuc
+        m.i = 15;
+    }
+
+    public static void main(String[] args) {
+        Number n = new Number();
+        n.i = 14;
+        f(n); //n.i = 15 do gọi lai phuong thuc f o tren
+        System.out.println(n.i);
+    }
+}
