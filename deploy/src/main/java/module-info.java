8d7e87b2ca9664ebf417face72d1605ae03ac041
Nguyen1976 <nguyen2202794@gmail.com>
2024-09-15 17:12:06 +0700

    Add project biuld intelij

--- deploy/src/main/java/module-info.java
+++ deploy/src/main/java/module-info.java
@@ -1,7 +1,8 @@
-module com.finalproj {
+module com.finalproj.deploy {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.finalproj to javafx.fxml;
-    exports com.finalproj;
+
+    opens com.finalproj.deploy to javafx.fxml;
+    exports com.finalproj.deploy;
 }
