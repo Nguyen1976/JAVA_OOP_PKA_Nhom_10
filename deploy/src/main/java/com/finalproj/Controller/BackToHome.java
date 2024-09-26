3a115db82c1455a3cdadab5f6e03c9ba9dec82df
Nguyen1976 <nguyen2202794@gmail.com>
2024-09-26 22:36:01 +0700

    Add logOut function javafx

---
+++ deploy/src/main/java/com/finalproj/Controller/BackToHome.java
@@ -0,0 +1,26 @@
+package com.finalproj.Controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class BackToHome {
+    public void goToHome(Stage currentStage) throws IOException {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/finalproj/HospitalView.fxml"));
+            Parent homeView = loader.load();
+
+            Scene homeScene = new Scene(homeView);
+
+            currentStage.setScene(homeScene);
+
+            currentStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
