4c40cce5030a76db6d7b1bf726fb08e258cf8cb1
DucHuy74 <duchuy74205@gmail.com>
2024-10-04 11:47:28 +0700

    add Login with JDCB

--- deploy/src/main/java/com/finalproj/Controller/LoginViewController.java
+++ deploy/src/main/java/com/finalproj/Controller/LoginViewController.java
@@ -1,5 +1,6 @@
 package com.finalproj.Controller;
 
+import com.finalproj.JDBCConnection.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +12,10 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.io.InputStream;
 import java.util.Scanner;
 
@@ -20,42 +25,72 @@ public class LoginViewController {
     @FXML
     private TextField password;
 
-    private String ACCOUNT;
-    private String PASSWORD;
+//    private String ACCOUNT;
+//    private String PASSWORD;
 
-    // Đọc tài khoản và mật khẩu từ file trong package com.finalproj.datafiles
-    private void loadCredentials() throws IOException {
-        InputStream inputStream = getClass().getResourceAsStream("/com/finalproj/datafiles/credentials.txt");
+    //    // Đọc tài khoản và mật khẩu từ file trong package com.finalproj.datafiles
+//    private void loadCredentials() throws IOException {
+//        InputStream inputStream = getClass().getResourceAsStream("/com/finalproj/datafiles/credentials.txt");
+//
+//        if (inputStream == null) {
+//            showAlert(Alert.AlertType.ERROR, "File không tìm thấy", "Không thể tìm thấy file credentials.txt.");
+//        } else {
+//            Scanner scanner = new Scanner(inputStream);
+//            if (scanner.hasNextLine()) {
+//                String[] credentials = scanner.nextLine().split(" ");
+//                if (credentials.length == 2) {
+//                    ACCOUNT = credentials[0];
+//                    PASSWORD = credentials[1];
+//                }
+//            }
+//            scanner.close();
+//        }
+//    }
+    private boolean handleLogin() {
+        String inputAccount = account.getText();
+        String inputPassword = password.getText();
 
-        if (inputStream == null) {
-            showAlert(Alert.AlertType.ERROR, "File không tìm thấy", "Không thể tìm thấy file credentials.txt.");
-        } else {
-            Scanner scanner = new Scanner(inputStream);
-            if (scanner.hasNextLine()) {
-                String[] credentials = scanner.nextLine().split(" ");
-                if (credentials.length == 2) {
-                    ACCOUNT = credentials[0];
-                    PASSWORD = credentials[1];
-                }
+        // Kết nối tới database và kiểm tra tài khoản
+        try (Connection connection = DBConnection.getConnection()) {
+            String query = "SELECT * FROM Login WHERE userName = ? AND password = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, inputAccount);
+            statement.setString(2, inputPassword);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                // Đăng nhập thành công
+                showAlert(Alert.AlertType.INFORMATION, "Đăng Nhập Thành Công", "Bạn đã đăng nhập thành công.");
+                return true;
+            } else {
+                // Đăng nhập thất bại
+                showAlert(Alert.AlertType.ERROR, "Đăng Nhập Thất Bại", "Tên đăng nhập hoặc mật khẩu không chính xác.");
+                return false;
             }
-            scanner.close();
-        }
-    }
 
-
-    // Kiểm tra đăng nhập
-    private boolean handleLogin() throws IOException {
-        loadCredentials();
-        if (account.getText().equals(ACCOUNT) && password.getText().equals(PASSWORD)) {
-            // Đăng nhập thành công
-            showAlert(Alert.AlertType.INFORMATION, "Đăng Nhập Thành Công", "Bạn đã đăng nhập thành công.");
-            return true;
-        } else {
-            // Đăng nhập thất bại
-            showAlert(Alert.AlertType.ERROR, "Đăng Nhập Thất Bại", "Tên đăng nhập hoặc mật khẩu không chính xác.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            showAlert(Alert.AlertType.ERROR, "Lỗi Cơ Sở Dữ Liệu", "Có lỗi khi kết nối tới cơ sở dữ liệu.");
             return false;
         }
     }
+
+
+
+    // Kiểm tra đăng nhập
+//    private boolean handleLogin() throws IOException {
+//        loadCredentials();
+//        if (account.getText().equals(ACCOUNT) && password.getText().equals(PASSWORD)) {
+//            // Đăng nhập thành công
+//            showAlert(Alert.AlertType.INFORMATION, "Đăng Nhập Thành Công", "Bạn đã đăng nhập thành công.");
+//            return true;
+//        } else {
+//            // Đăng nhập thất bại
+//            showAlert(Alert.AlertType.ERROR, "Đăng Nhập Thất Bại", "Tên đăng nhập hoặc mật khẩu không chính xác.");
+//            return false;
+//        }
+//    }
 
     private void showAlert(Alert.AlertType alertType, String title, String content) {
         Alert alert = new Alert(alertType);
